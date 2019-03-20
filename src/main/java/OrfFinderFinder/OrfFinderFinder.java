c54d373c965c6233a757d2e4349c2e3f974c0c1d
Elco van Rijswijk <elcovanrijswijk@hotmail. com>
2019-03-20 11:28:24 +0100

    Gui in kleine letters

--- src/main/java/OrfFinderFinder/OrfFinderFinder.java
+++ src/main/java/OrfFinderFinder/OrfFinderFinder.java
@@ -1,14 +1,40 @@
 package OrfFinderFinder;
 
 
+import orFinderApp.Query;
+import org.biojava.bio.seq.FramedFeature;
+import org.biojava.bio.seq.Sequence;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class OrfFinderFinder {
-    //final List<String> list = Arrays.asList(new String[]{"foo", "bar"});
+    final List<String> stop_codons = Arrays.asList("tga","tag","taa");
+    final String start_codon = "atg";
 
-    //final String stop_codons=["tga","TAG","TAA"]
+    List<Integer> start_positions = new ArrayList<>();
+    List<Integer> stop_positions = new ArrayList<>();
+    Integer count_start=0;
+    Integer count_stop=0;
 
-    public void FindOrf(String Sequence){
-        System.out.println("poah");
+    public static String Sequence="ATGGTGTTGTGTGTGTGTGGGGGGGGGGGGGGGGGGGTGTGTGTGTTGTGTGTGTTGTTAAATGTGATGTACCACACCCTACCTTCCTAAACTCCTGGGAGCAAGATGACGGTTGTGGACCGTGGAGCGACACGCGACGCTCTGCGTCTCGG";
+
+    public static void main(String[] args){
+        FindOrf(Sequence);
+    }
+
+    private static void FindOrf(String Sequence){
+        ArrayList<String> Frames = ReadingFrames();
+
+
+    }
+    private static ArrayList<String> ReadingFrames()    {
+        ArrayList<String> Frames = new ArrayList<String>();
+
+
+
+        return Frames;
+
     }
 }
