a326f4aeb159047fb9fbfb2c74e75b391bf7da8e
Elco van Rijswijk <elcovanrijswijk@hotmail. com>
2019-03-29 11:04:32 +0100

    OrfFinder 1.0

--- src/main/java/OrfFinderFinder/OrfFinderFinder.java
+++ src/main/java/OrfFinderFinder/OrfFinderFinder.java
@@ -1,15 +1,13 @@
 package OrfFinderFinder;
 
-
 import orFinderApp.ORF;
 import orFinderApp.Query;
-
-import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * ORF finder finds Genes on the basis of start and stop codons
  * Class excepts Query finds start and stops
- * The Query adds an ORF object with the start and stop position of the founded gene
+ * The Query adds an ORF object with the start and stop position of the founded ORF
  */
 public class OrfFinderFinder {
 
@@ -22,36 +20,28 @@ public class OrfFinderFinder {
     Boolean Continue = true;
 
     /**
-     * HandleQuery calls function GenreadingFrame and findGenes
-     *
-     * @param query
+     * HandleQuery handles sequence from query makes and calls FindGenes with forward and reverse strand.
+     * @param query with header and sequence
      */
     public void HandleQuery(Query query) {
 
-        ArrayList<String> frames = GenReadingFrame(query.getSequence());
-
-        String RevSeq = new StringBuilder(query.getSequence()).reverse().toString();
-        String ForSeq = query.getSequence();
-
-        FindGenes(ForSeq, query);
+        FindGenes(query.getSequence(), query);
         Continue = true;
         Forward = false;
-        FindGenes(RevSeq, query);
-
+        FindGenes(new StringBuilder(query.getSequence()).reverse().toString(), query);
 
     }
 
     /**
-     * FindGenes finds start codons and if a gene is found start and stop position are added
-     * to the Query
-     *
+     * FindGenes finds start codons and calls FindStopCodon to find a stop codon
+     * FindGenes also checks if founded ORF >100 nucleotides
+     * FindGenes also checks if StopCodon isn't already present so nested ORF are not counted as new genes
      * @param frame Is being searched for start codons
      * @param query Where if a Gene is found start and stop position will be added to that query
-     * @return Allgenes where all founded genes are stored in as Strings (not active!!)
      */
-    public ArrayList<String> FindGenes(String frame, Query query) {
+    public void FindGenes(String frame, Query query) {
 
-        ArrayList<String> allGenes = new ArrayList<String>();
+        Stack<Integer> StopCodon = new Stack<>();
 
         int start = 0;
         int stop = 0;
@@ -61,95 +51,55 @@ public class OrfFinderFinder {
             start = frame.indexOf(startCodon, start + 1);
             if (start < 0) {
                 Continue = false;
-            }
-
-            int already = stop - start;
-
-            if (already > 0 && already % 3 == 0) {
-
             } else {
+                stop = FindStopCodon(frame, start);
 
-                if (start > 0) {
-                    stop = FindStopCodon(frame, start);
+                if ((stop - start > 100) && !StopCodon.contains(stop)) {
+                    StopCodon.push(stop);
 
-                    if ((stop - start > 100)) {
-                        //String gene = frame.substring(start, stop + 3);
-                        //if (!allGenes.contains(gene)) {
-                        //  allGenes.add(gene);
-                        //}
-                        if (Forward) {
-                            query.addOrfList(new ORF(start, (stop + 3)));
-                        }
-
-                        if (!Forward) {
-                            int startfor = (frame.length() - start);
-                            int stopfor = (frame.length() - stop);
-                            query.addOrfList(new ORF(startfor, (stopfor - 3)));
-                        }
-                        System.out.println("From: " + start + " to " + stop + " Gene: ");
+                    if (Forward) {
+                        query.addOrfList(new ORF((start + 1), (stop + 3)));
+                    }
+                    if (!Forward) {
+                        int startFor = (frame.length() - start);
+                        int stopFor = (frame.length() - stop);
+                        query.addOrfList(new ORF((startFor - 1), (stopFor - 3)));
                     }
                 }
             }
         }
 
-        return allGenes;
-
-
     }
-
 
     /**
      * FindStopCodon accepts the frame with its start position and if a stop codon is found
      * returns the stop position
      *
-     * @param frame the String frame from FindGenes
-     * @param start Integer start position from FindGenes
+     * @param frame the String frame of the DNA sequence from FindGenes
+     * @param start Integer start position of startcodon on the frame from FindGenes
      * @return stop position of founded gene
      */
     public int FindStopCodon(String frame, int start) {
 
         for (int i = start; i < frame.length(); i += 3) {
 
-            if ((i + 3) < frame.length()) {
-
+            if ((i + 90) > frame.length()) {
+                Continue = false;
+            } else {
                 String frameString = frame.substring(i, i + 3);
 
-                if (frameString.equals(stop1Codon)) {
-                    return i;
-                } else if (frameString.equals(stop2Codon)) {
-                    return i;
+                switch (frameString) {
+                    case stop1Codon:
+                        return i;
+                    case stop2Codon:
+                        return i;
 
-                } else if (frameString.equals(stop3Codon)) {
-                    return i;
+                    case stop3Codon:
+                        return i;
                 }
             }
-            if ((i + 3) > frame.length()) {
-                Continue = false;
-            }
-
         }
         return -1;
-
     }
-
-    /**
-     * GenReadingFrame accepts the sequence and returns the forward and reverse strand of the sequence
-     *
-     * @param Sequence from HandleQuery
-     * @return Arraylist with the forward and reverse strand
-     */
-    public ArrayList<String> GenReadingFrame(String Sequence) {
-
-        ArrayList<String> frames = new ArrayList();
-
-        String ForSeq = Sequence;
-        String RevSeq = new StringBuilder(Sequence).reverse().toString();
-
-        frames.add(ForSeq);
-        frames.add(RevSeq);
-
-        return frames;
-    }
-
 
 }
